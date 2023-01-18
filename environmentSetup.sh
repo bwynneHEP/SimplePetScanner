7e40317535892a7455eec5062922f190fc30d6b7
Ben Wynne <bwynne@cern.ch>
2023-01-18 13:18:26 +0000

    G4 version bump

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,2 +1,2 @@
-GEANT4_myPath=/home/ben/Software/geant/geant4.10.04.p02
-source $GEANT4_myPath/share/Geant4-10.4.2/geant4make/geant4make.sh
+GEANT4_myPath=/home/ben/Software/geant/geant4-v11.0.2
+source $GEANT4_myPath/share/Geant4-11.0.2/geant4make/geant4make.sh
