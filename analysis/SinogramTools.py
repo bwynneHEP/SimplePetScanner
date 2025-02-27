65b1b553eaae92f7bfc8d5946138537c28dab73b
Hanna Borecka-Bielska <borecka.hanna@gmail.com>
2025-02-27 23:21:56 +0000

    Use constants instead of indices

--- analysis/SinogramTools.py
+++ analysis/SinogramTools.py
@@ -1,11 +1,12 @@
 import numpy as np
+from SimulationDataset import *
 
 def CalcSinogramCoords(event):
-    x1 = event[0][4]*np.cos(event[0][5])
-    y1 = event[0][4]*np.sin(event[0][5])
+    x1 = event[0][DATASET_R]*np.cos(event[0][DATASET_PHI])
+    y1 = event[0][DATASET_R]*np.sin(event[0][DATASET_PHI])
     
-    x2 = event[1][4]*np.cos(event[1][5])
-    y2 = event[1][4]*np.sin(event[1][5])
+    x2 = event[1][DATASET_R]*np.cos(event[1][DATASET_PHI])
+    y2 = event[1][DATASET_R]*np.sin(event[1][DATASET_PHI])
 
     sinogramTheta = np.atan2(x1 - x2, y1 - y2)
 
